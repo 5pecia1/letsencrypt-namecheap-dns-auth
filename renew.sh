4cfc2231af0aecc4454d54f60bb97bff7c21352e
5pecia1 <pdpxpd@gmail.com>
2022-01-31 12:47:21 +0900

    fix about wildcard

--- renew.sh
+++ renew.sh
@@ -7,6 +7,14 @@ if [[ "$DRY_RUN" == "true" ]]; then
     TEST_OPTION="--dry-run --debug"
     SLEEP_TIME=1m
 fi
+
+HOST=$(echo ${CERT_DOMAIN} | rev | cut -d. -f3- | rev)
+if [[ "$HOST" == "*" ]]; then
+    TLD=$(echo ${CERT_DOMAIN} | rev | cut -d. -f1 | rev)
+    SLD=$(echo ${CERT_DOMAIN} | rev | cut -d. -f2 | rev)
+    CERT_DOMAIN=${SLD}.${TLD}
+fi
+
 trap exit TERM
 while :; do
     certbot $TEST_OPTION --text --agree-tos --email ${CERT_EMAIL} --cert-name ${CERT_DOMAIN} --manual --preferred-challenges dns --expand --renew-by-default --non-interactive --manual-public-ip-logging-ok renew --manual-auth-hook=/home/letsencrypt-namecheap-dns-auth.sh
